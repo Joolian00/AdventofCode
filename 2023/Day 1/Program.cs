3d3265688735aeb5c4a506fb72e7ebcbb3b406ef
Joolian00 <97477118+Joolian00@users.noreply.github.com>
2023-12-10 21:42:20 +0100

    First Commit

--- 2023/Day 1/Program.cs
+++ 2023/Day 1/Program.cs
@@ -1,6 +1,9 @@
-﻿namespace Day_1 {
+﻿using System;
+
+namespace Day_1 {
     internal class Program {
         public static void Main() {
+            Console.WriteLine("Hello");
         }
     }
 }
