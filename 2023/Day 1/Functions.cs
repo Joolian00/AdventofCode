82ddf5a4de344daddcdcd5c648efdd8892c5bcee
Joolian00 <97477118+Joolian00@users.noreply.github.com>
2023-12-11 00:20:32 +0100

    Day 1

---
+++ 2023/Day 1/Functions.cs
@@ -0,0 +1,77 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day_1;
+
+public static class Functions {
+    public static int PartOne(string[] lines) {
+        string pattern = @"\d";
+
+        List<int> calibrationValues = [];
+        int finalCalibrationValue = 0;
+        
+        foreach (var line in lines) {
+            MatchCollection matches = Regex.Matches(line, pattern);
+            string calibrationValueString = matches[0].Value + matches[^1].Value;
+            calibrationValues.Add(int.Parse(calibrationValueString));
+        }
+
+        foreach (var value in calibrationValues) {
+            finalCalibrationValue += value;
+        }
+        
+        return finalCalibrationValue;
+    }
+
+    public static int PartTwo(string[] lines) {
+        List<int> calibrationValues = [];
+        int finalCalibrationValue = 0;
+
+        string pattern = @"\d|one|two|three|four|five|six|seven|eight|nine";
+
+        Dictionary<string, string> wordToNumberMap = new Dictionary<string, string> {
+            {"one", "1"},
+            {"two", "2"},
+            {"three", "3"},
+            {"four", "4"},
+            {"five", "5"},
+            {"six", "6"},
+            {"seven", "7"},
+            {"eight", "8"},
+            {"nine", "9"}
+        };
+        
+        foreach (var line in lines) {
+            MatchCollection matches = Regex.Matches(line, pattern);
+            
+            Console.WriteLine(line);
+            
+            
+            if (wordToNumberMap.TryGetValue(matches[0].Value, out string value1)) {
+                
+            }
+            else {
+                value1 = matches[0].Value;
+            }
+
+            if (wordToNumberMap.TryGetValue(matches[^1].Value, out string value2)) {
+                
+            }
+            else {
+                value2 = matches[^1].Value;
+            }
+            Console.WriteLine(value1 + " and " + value2);
+            
+            calibrationValues.Add(int.Parse(value1+value2));
+            
+            //string calibrationValueString = matches[0].Value + matches[^1].Value;
+            //calibrationValues.Add(int.Parse(calibrationValueString));
+        }
+        
+        foreach (var value in calibrationValues) {
+            finalCalibrationValue += value;
+        }
+
+        return finalCalibrationValue;
+    }
+
+}
--- 2023/Day 1/Program.cs
+++ 2023/Day 1/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
+using Day_1;
 
-Console.WriteLine("Hello, World!");
+List<string> FileSelection = ["testInput.txt", "input.txt"];
+
+string[] lines = File.ReadAllLines(FileSelection[1]);
+
+
+PrintHorizontalLine('-', 30);
+var watch = Stopwatch.StartNew();
+
+Console.WriteLine($"Solution for Part one: {Functions.PartOne(lines)} {ElapsedTime()}");
+Console.WriteLine($"Solution for Part two: {Functions.PartTwo(lines)} {ElapsedTime()}");
+
+PrintHorizontalLine('-', 30);
+return;
+
+static void PrintHorizontalLine(char character, int length)
+{
+    string line = new(character, length);
+    Console.WriteLine(line);
+}
+
+string ElapsedTime() {
+    var elapsed = watch.ElapsedMilliseconds;
+    watch.Restart();
+
+    return $"{elapsed}ms";
+}
+
+
+
