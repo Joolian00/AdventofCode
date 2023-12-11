8d20b20d0b67a2a3bed9c377d57375dfece05530
fhs49044 <fhs49044@fh-salzburg.ac.at>
2023-12-11 12:52:50 +0100

    Second day start

---
+++ 2023/Day 2/Functions.cs
@@ -0,0 +1,38 @@
+﻿﻿using System.Text.RegularExpressions;
+
+namespace Day_2;
+
+public static class Functions {
+    public static int PartOne(string[] lines) {
+        string[] patterns = new[]
+        {
+            @"(\d+) (blue)",  
+            @"(\d+) (red)",
+            @"(\d+) (green)"
+        };
+
+        foreach (var line in lines) {
+            Console.WriteLine(line);
+            
+            foreach (var pattern in patterns) {
+                MatchCollection matches = Regex.Matches(line, pattern);
+                int totalAmount = 0;
+                
+                
+                
+            }
+            
+        }
+
+        
+        
+        return 0;
+    }
+
+    public static int PartTwo(IEnumerable<string> lines) {
+        
+        
+        return 0;
+    }
+
+}
