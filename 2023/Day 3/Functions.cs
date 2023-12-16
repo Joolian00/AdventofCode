cb1883befaa97222dcbc2943789ee20ad0c1f5da
Joolian00 <97477118+Joolian00@users.noreply.github.com>
2023-12-16 21:08:50 +0100

    Setup for Day 3

---
+++ 2023/Day 3/Functions.cs
@@ -0,0 +1,18 @@
+﻿﻿using System.Text.RegularExpressions;
+
+namespace Day_1;
+
+public static class Functions {
+    public static int PartOne(string[] lines) {
+
+        
+        return 0;
+    }
+
+    public static int PartTwo(string[] lines) {
+    
+        
+        return 0;
+    }
+
+}
